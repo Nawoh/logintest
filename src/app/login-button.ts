9119a813a01420ef793f48221b6dd5381151b90b
Nawoh <53145699+Nawoh@users.noreply.github.com>
2022-12-10 15:34:36 +0100

    2 files modified

--- src/app/login-button.ts
+++ src/app/login-button.ts
@@ -1,6 +1,5 @@
 //Button not rendered and Error
 
-/*+
 import { Component } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 
@@ -15,27 +14,5 @@ export class LoginButtonComponent {
     this.auth.loginWithRedirect();
   }
 }
-*/
 
-//Button rendered without Error
 
-import { Component } from '@angular/core';
-import { AuthService } from '@auth0/auth0-angular';
-
-@Component({
-  selector: 'app-login-button',
-  template: '<button (click)="login()">Log in</button>',
-})
-export class LoginButtonComponent {
-  constructor() {}
-
-  //Maybe the constructor here is the problem.
-  //AuthService is a singleton in app.module and we try to built a new instance of it with
-  //private auth: AuthService here ......   maybe ???
-  //if so i still dont know how to fix it
-
-  login() {
-    console.log('The button itself is not the problem');
-    //this.auth.loginWithRedirect();
-  }
-}
